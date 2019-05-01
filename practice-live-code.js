01ed54cd7c890722eeed2afe2ed2a9a9a6593238
aoelbaum <ashleyoelbaum@gmail.com>
2019-05-01 16:15:39 -0400

    question 1 updated syntax

--- practice-live-code.js
+++ practice-live-code.js
@@ -1,5 +1,5 @@
-function isDivisible (int){
-    if (int % 3 === 0 || int % 5){
+function isDivisible (int) {
+    if (int % 3 === 0 || int % 5 === 0) {
         return true;
     }
     return false;
